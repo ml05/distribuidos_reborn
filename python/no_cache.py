72434f25c10da38f9f5e99261f2a408dc73e196f
ml05 <matias.aguilera_l@mail.udp.cl>
2023-04-08 17:44:38 -0400

    update

--- python/no_cache.py
+++ python/no_cache.py
@@ -1,38 +1,32 @@
 import requests
 import json
 import time
+from flask import Flask, request, jsonify
 
 def searchPerson(query):
-    start_time = time.time()
     # realiza la consulta a la API
     url = f'https://swapi.dev/api/people/?search={query}'
     response = requests.get(url)
     # si la consulta es exitosa la retorna
     if response.status_code == 200:
         results = response.json()
-        end_time = time.time()
-        response_time = end_time - start_time
-        return results, response_time
+        return results
     else:
-        end_time = time.time()
-        response_time = end_time - start_time
-        return None, response_time
+        return None
 
-# este parametro se puede cambiar para probar el cache con otras consultas
-query = 'luke'
-results = searchPerson(query)[0]
-# if results:
-#     for result in results['results']:
-#         print(result['name'])
-#         print(result['films'])
-#         print(result['url'])
-# else:
-#     print('No se encontraron resultados para su búsqueda.')
+# REST API
+app = Flask(__name__) # crea instancia FLASK
 
-# toma de tiempo en ejecucion de una consulta
-response_time = searchPerson(query)[1]
+@app.route('/search', methods=['GET']) #Define la ruta para la API REST de busqueda
+def search():
+    query = request.args.get('query') # obtiene la query de la solicitud GET
+    if not query:
+        return jsonify({'error': 'Parametro de busqueda requerido'}), 400 # si no se especifica paramentro "error"
+    results = searchPerson(query) #hace busqueda
+    if results:
+        return jsonify(results) # si se encuentra, retorna json
+    else:
+        return jsonify({'error': 'No se encontraron resultados para su búsqueda.'}), 404 #error si no se encuentra
 
-print(f'Tiempo de respuesta: {response_time:.2f} segundos')
-if response_time > 0.05:
-    # Si el tiempo de respuesta es mayor a 5 segundos, registra este tiempo en un archivo de log o en una base de datos
-    print('El tiempo de respuesta es mayor a 5 segundos. Se debe investigar.')
+if __name__ == '__main__': #ejecuta la aplicacion si se ejecuta el archivo
+    app.run()
