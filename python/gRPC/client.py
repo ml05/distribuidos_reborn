58f7da4c656ae40c7dead517c6bb4b544d35b7b8
ShunZhouZ <shun.zhou_z@mail.udp.cl>
2023-04-09 11:50:54 -0400

    Update client.py

--- python/gRPC/client.py
+++ python/gRPC/client.py
@@ -5,7 +5,7 @@ import swapi_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = swapi_pb2_grpc.SwapiServiceStub(channel)
-        response = stub.GetPerson(swapi_pb2.GetPersonRequest(id=1))
+        response = stub.GetPerson(swapi_pb2.GetPersonRequest(id=3))
     print(response)
 
 if __name__ == '__main__':
