83d7058e0a324c0eaf081dba75a96876080d72ae
ml05 <matias.aguilera_l@mail.udp.cl>
2023-04-10 17:36:30 -0400

    update dataset

--- dataset/generarDataset.py
+++ dataset/generarDataset.py
@@ -4,28 +4,28 @@
 import pandas as pd
 import random
 
-# Cargar el archivo CSV
-datos = pd.read_csv('/Users/matiasaguileralienlaff/Documents/distribuidos/distribuidos_reborn/dataset/starwars.csv')
+# # Cargar el archivo CSV
+# datos = pd.read_csv('/Users/matiasaguileralienlaff/Documents/distribuidos/distribuidos_reborn/dataset/starwars.csv')
 
-# Generar una lista de 1000 números aleatorios enteros entre 0 y 51
-indices = [random.randint(0, 51) for i in range(1000)]
+# # Generar una lista de 1000 números aleatorios enteros entre 0 y 51
+# indices = [random.randint(0, 146) for i in range(3000)]
 
-# Crear una nueva lista vacía para la muestra
-muestra = []
+# # Crear una nueva lista vacía para la muestra
+# muestra = []
 
-# Iterar a través de los números aleatorios y agregar el valor de la fila correspondiente a la lista de muestra
-for i in indices:
-    muestra.append(datos.iloc[i])
+# # Iterar a través de los números aleatorios y agregar el valor de la fila correspondiente a la lista de muestra
+# for i in indices:
+#     muestra.append(datos.iloc[i])
 
-# Escribir la lista de muestra en un nuevo archivo CSV
-pd.DataFrame(muestra).to_csv('test.csv', index=False)
+# # Escribir la lista de muestra en un nuevo archivo CSV
+# pd.DataFrame(muestra).to_csv('test.csv', index=False)
 
 # CALCULAR LA FRECUENCIA DE CADA PERSONAJE EN EL DATASET
 # Lee el archivo CSV en un DataFrame de pandas
 df = pd.read_csv('/Users/matiasaguileralienlaff/Documents/distribuidos/distribuidos_reborn/dataset.csv')
 
 # Obtiene la frecuencia de cada valor en una columna específica del DataFrame
-frequencies = df['character'].value_counts()
+frequencies = df['field'].value_counts()
 
 # Crea un archivo de texto y escribe las frecuencias de los valores en él
 with open('frecuencias.txt', 'w') as f:
