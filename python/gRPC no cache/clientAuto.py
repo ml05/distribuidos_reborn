9c878062922c073234981194e1ab98c56223c2be
ShunZhouZ <shun.zhou_z@mail.udp.cl>
2023-04-11 16:54:03 -0400

    gRPC + csv time writer

--- python/gRPC no cache/clientAuto.py
+++ python/gRPC no cache/clientAuto.py
@@ -7,9 +7,12 @@ import time
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = swapi_pb2_grpc.SwapiServiceStub(channel)
-        with open('dataset_ids.csv') as csvfile:
+        with open('dataset_ids.csv') as csvfile, open('tiempos.csv','w',newline='') as outfile:
             reader = csv.DictReader(csvfile)
-            
+            writer = csv.writer(outfile)
+        
+            writer.writerow(['field','query','time'])
+
             for row in reader:
                 start_time = time.time()
                 field = row['field']
@@ -26,8 +29,9 @@ def run():
                     response = stub.GetSpecies(swapi_pb2.GetSpeciesRequest(id=query))
                     print(response)
                 end_time = time.time()
-                elapsed_time = end_time - start_time
-                print(f"tiempo: {elapsed_time:.4f} segundos")
+                tiempo = end_time - start_time
+                print(f"tiempo: {tiempo:.4f} segundos")
+                writer.writerow([field, query, tiempo])
                     
 
 if __name__ == '__main__':
