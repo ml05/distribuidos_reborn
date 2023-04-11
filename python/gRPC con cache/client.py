f3e4c382bb4ceba52392bf4e4d89f8e3c9d21e76
ShunZhouZ <shun.zhou_z@mail.udp.cl>
2023-04-10 23:43:08 -0400

    gRPC cliente con lectura automatica

--- python/gRPC con cache/client.py
+++ python/gRPC con cache/client.py
@@ -1,12 +1,33 @@
 import grpc
 import swapi_pb2
 import swapi_pb2_grpc
+import csv
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = swapi_pb2_grpc.SwapiServiceStub(channel)
-        response = stub.GetPerson(swapi_pb2.GetPersonRequest(id=3))
-    print(response)
+        with open('dataset_id.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            contador = 2
+            for row in reader:
+                
+                field = row['field']
+                query = int(row['query'])
+                if field == 'planets':
+                    response = stub.GetPlanet(swapi_pb2.GetPlanetRequest(id=query))
+                    print(response)
+                    contador=contador+1
+                    print (contador)
+                elif field == 'people':
+                    response = stub.GetPerson(swapi_pb2.GetPersonRequest(id=query))
+                    print(response)
+                    contador=contador+1
+                    print (contador)
+                elif field == 'species':
+                    response = stub.GetSpecies(swapi_pb2.GetSpeciesRequest(id=query))
+                    print(response)
+                    contador=contador+1
+                    print (contador)
 
 if __name__ == '__main__':
     run()
